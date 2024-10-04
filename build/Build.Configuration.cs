c2049f760847fec98078f2d4b060436e0a9ebfbb
Omkar Bhagwat <ombhagwat29@gmail.com>
2024-10-04 12:15:29 +0200

    s

--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -1,6 +1,6 @@
 ﻿sealed partial class Build
 {
-    const string Version = "1.0.2";
+    const string Version = "0.0.0";
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
     readonly AbsolutePath ChangeLogPath = RootDirectory / "Changelog.md";
 
--- source/SharpRevit.Tools/Application.cs
+++ source/SharpRevit.Tools/Application.cs
@@ -2,7 +2,7 @@
 using SharpRevit.Commands;
 using Nice3point.Revit.Toolkit.External;
 
-namespace SharpRevit
+namespace SharpRevit.Tools
 {
     /// <summary>
     /// Application Entry Point
@@ -21,23 +21,16 @@ namespace SharpRevit
                 Application.CreateRibbonTab(tabName);
 
                 string parameterPanelName = "Parameters";
-                string validationPanelName = "Apartment Validation";
+  
                 string GeneralPanelName = "General";
-                string accPanelName = "ACC";
+   
 
-               var parametersPanel = Application.CreatePanel(parameterPanelName, tabName);
-
-                var validationPanel = Application.CreatePanel(validationPanelName, tabName);
+                var parametersPanel = Application.CreatePanel(parameterPanelName, tabName);
 
                 var generalPanel = Application.CreatePanel(GeneralPanelName, tabName);
 
 
-                var accPanel = Application.CreatePanel(accPanelName, tabName);
-
-
                 FamilyParameters_Command.CreateFamilyParametersButton(parametersPanel);
-
-
 
                 CreateKeynotes_Command.CreateKeynotesCreationButton(generalPanel);
 
